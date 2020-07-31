c0566c90cf217bc7b6df48b6bd2fcbc3908d3cb0
Tarun <tarunharidasan2003@gmail.com>
2020-07-31 16:21:01 -0400

    Dynamically load js and json files

--- bot.js
+++ bot.js
@@ -4,27 +4,18 @@ const opus = require('opusscript');
 const colors = require('colors');
 const client = new Discord.Client();
 const util = require("util");
-commands = {}
-system = {}
-data = {}
-utils = {}
+let commands = {}
+let system = {}
+let data = {}
+let utils = {}
 let dispatachers = [];
 const fs = require('fs');
 
+/*<--------------------Loading------------------------->*/
+commands = require("./handler/commands.js")(fs);
+system = require("./handler/system.js")(fs);
+utils = require("./handler/utils.js")(fs);
+data = require("./handler/data.js")(fs);
 
-let files;
-let loadStr;
-/*<--------------------Dynamic Command File Loader------------------------->*/ 
-files = fs.readdirSync("./commands/").filter(f => f.split(".").pop() === "js");
-loadStr = '[Commands]';
-if(files.length <= 0) console.log(`${loadStr.green.bold} No commands to load.\n`);
-else {
-    console.log(`${loadStr.green.bold} ${files.length} commands detected.`);
-    files.forEach((f, i) => {
-        let props = require(`./commands/${f}`);
-        let loaded = `${f.toString()} loaded!`;
-        console.log(`${loadStr.green.bold} [${i+1}] ${loaded.yellow}`);
-        commands[props.help.name] = props;
-    });
-    console.log(`${loadStr.green.bold} All commands have been loaded!\n`);  
-}
+
+
