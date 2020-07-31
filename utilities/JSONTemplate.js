e69a0cb0a54d6203369a1cac80a0d05c5c23c109
Tarun <tarunharidasan2003@gmail.com>
2020-07-31 19:45:09 -0400

    Added a wrapper for JSON data

---
+++ utilities/JSONTemplate.js
@@ -0,0 +1,12 @@
+class JSONTemplate {
+	constructor(fs, fp, data) {
+		this.fs = fs; //fs object
+		this.fp = fp; //File path
+		this.data = data; //Actual json data
+	}
+	write() {
+		this.fs.writeFileSync(this.fp, JSON.stringify(this.data, null, 2));
+	}
+}
+module.exports.run = JSONTemplate;
+module.exports.name = "JSONTemplate";
