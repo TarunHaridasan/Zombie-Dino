86c8f6eea35147ecd79db6248b636a720a2dd192
Tarun <tarunharidasan2003@gmail.com>
2020-07-31 16:18:55 -0400

    Added dynamic system loader

---
+++ handler/system.js
@@ -0,0 +1,18 @@
+function run(fs) {
+	let files = fs.readdirSync("./system/").filter(f => f.split(".").pop() === "js");
+	let loadStr = '[System]';
+	let system ={}
+	if(files.length <= 0) console.log(`${loadStr.yellow.bold} No system files to load.\n`);
+	else {
+	    console.log(`${loadStr.yellow.bold} ${files.length} system files found.`);
+	    files.forEach((f, i) => {
+	        let props = require(`../system/${f}`);
+	        let loaded = `${f} loaded!`
+	        console.log(`${loadStr.yellow.bold} [${i+1}] ${loaded.grey}`);
+	        system[props.help.name] = props;
+	    });
+	    console.log(`${loadStr.yellow.bold} All system files have been loaded!\n`);
+	}
+	return system;
+}
+module.exports = run;
