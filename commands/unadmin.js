aa444c7b4de9b119dc2579d11ed6b8aa0919c123
Tarun <tarunharidasan2003@gmail.com>
2020-08-01 19:03:24 -0400

    Added ability to unadmin a member

---
+++ commands/unadmin.js
@@ -0,0 +1,60 @@
+module.exports.run = async (client, message, args) => {
+    //Extract variables
+    let userID = message.author.id;
+    let serverID = message.guild.id;
+    let adminTarget = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+
+    //Instantiate object of ServerData class
+    let Server = require("../utilities/server");
+    let server = new Server(serverID);
+
+    //Only an admin can admin others
+    if (!server.isAdmin(userID)) {
+        message.channel.send({embed: {
+            color: 0xff0000,
+            description: `<@${userID}> This is a whitelisted command.`
+        }});
+        return;
+    }
+
+    //Target must exist
+    if (!adminTarget) {
+        message.channel.send({embed: {
+            color: 0xff0000,
+            description: `<@${userID}> Target not found.`
+        }});
+        return;
+    }
+
+    //Target must be an admin
+    if (!server.isAdmin(adminTarget.id)) {
+        message.channel.send({embed: {
+            color: 0xff0000,
+            description: `<@${userID}> Target is not an admin`
+        }});
+        return;
+    }
+    
+    //User cannot unadmin self
+    if (adminTarget.id == userID) {
+        message.channel.send({embed: {
+            color: 0xff0000,
+            description: `<@${userID}> You cannot unadmin yourself.`            
+        }});
+        return;
+    }
+
+    //Succesfull Demotion
+    message.channel.send({embed: {
+        color: 0x00FF00,
+        description: `<@${userID}> Successfully unadmined <@${adminTarget.id}>`
+    }});
+    server.remAdmin(adminTarget.id);
+}
+
+module.exports.help = {
+    name: ["unadmin"],
+    description: "This command is only used by the owner of the server to unadmin a member for debugging the bot.",
+    page: 7,
+    title: "Debugging Commands"
+};
