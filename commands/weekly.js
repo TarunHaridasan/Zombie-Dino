a7d59c6795efce0720f86392bf5875c222df3ef9
Jason <jasonsu894@gmail.com>
2020-08-01 21:02:54 -0400

    Bug fixes.

--- commands/weekly.js
+++ commands/weekly.js
@@ -8,39 +8,38 @@ module.exports.run = async (client, message, args) => {
     let userBal = new Money(userID, data['money.json']);
 
     let date = new Date();
-    let canClaim = new Date(user.get('dailyMS'));
+    let canClaim = new Date(user.get('weeklyMS'));
     let tmr = canClaim.setDate((canClaim.getDate()+1));
-    //Checking if the user has already claimed their daily gift!
+    //Checking if the user has already claimed their weekly gift!
     if(date.getTime() < canClaim.getTime()) {
-        let msg = '';
-        if(new Date(date).getDate() == new Date(user.get('dailyMS')).getDate()) msg = 'Come back later today';
-        else msg = 'Come back tomorrow';
         let suff = (canClaim.getHours() > 11 ? 'PM' : 'AM');
         let mins = canClaim.getMinutes().toLocaleString();
+        let dateStr = canClaim.toDateString();
+        dateStr = dateStr.slice(0, dateStr.length-5);
         let pre = (mins.length < 2 ? '0':'');
         mins = `${pre}${mins}`;
         message.channel.send({embed: {
             color: 0xff0000,
-            description: `<@${message.member.user.id}> You cannot claim your gift yet. ${msg} at ${timeArr[canClaim.getHours()]}:${mins}${suff}.`
+            description: `<@${message.member.user.id}> You cannot claim your gift yet. Come back on ${dateStr} at ${timeArr[canClaim.getHours()]}:${mins}${suff}.`
         }});
         return;
     }
     //Checking if the user has missed a day
-    if(date.getTime() >= tmr) user.reset('dailyStr');
-    let claimAmount = 1000+(750*+user.get('dailyStr'));
+    if(date.getTime() >= tmr) user.reset('weeklyStr');
+    let claimAmount = 10000+(7500*+user.get('weeklyStr'));
     userBal.add(claimAmount);
-    user.set('dailyClaimed', true);
-    user.set('dailyMS', (date.getTime()+86400000));
-    user.set('dailyStr', +user.get('dailyStr')+1);
+    user.set('weeklyClaimed', true);
+    user.set('weeklyMS', date.setDate(date.getDate()+6));
+    user.str('weeklyStr');
     message.channel.send({embed: {
         color: 0x00ff00,
-        description: `<@${message.member.user.id}> You just claimed your daily gift of ${claimAmount.toLocaleString()}! 🔥: ${user.get('dailyStr')}`
+        description: `<@${message.member.user.id}> You just claimed your weekly gift of ${claimAmount.toLocaleString()}! 🔥: ${user.get('weeklyStr')}`
     }});
 };
 
 module.exports.help = {
-    name: ["daily"],
-    description: "Claim your daily bonus!",
+    name: ["weekly"],
+    description: "Claim your weekly bonus!",
     page: 2,
     title: "Economy Commands"
 };
