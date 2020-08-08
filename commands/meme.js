36dbeaeba93d35afc32bde038419e33429c3063e
Tarun <tarunharidasan2003@gmail.com>
2020-08-08 16:36:42 -0400

    Updated meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -1,30 +1,17 @@
 module.exports.run = async (_client, message, args, _prefix) => {
     //Variables
     const fetch = require('node-fetch');
-    let url = "https://meme-api.herokuapp.com/gimme";
+    let meme = require("../utilities/meme.js")
+    let url = "https://meme-api.herokuapp.com/gimme"
 
-    //Make the request
-    let data = await fetch(url)
-    .then(data => data.json())
-
-    //Display to user
-    .then(data => {
-        message.reply({files: [data.url]})
-    })
-    
-    //On failiure
-    .catch(err => {
-        console.log(err);
-        message.reply({embed: {
-            color: 0xFF0000,
-            title: "Error:",
-            description: `Something went wrong...`
-        }});
-    })
+    //Retrieve memes
+    meme(url, message);
 }
 module.exports.help = {
     name: ["meme"],
-    description: "This command can be used to randomly find a meme",
+    description: "This command can be used to randomly find a meme from r/memes, r/dankmemes, and r/me_irl",
     page: 1,
     title: "Fun Commands"
-  }
+}
+
+
