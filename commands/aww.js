17b011d8e6d030c489dce7cd37c4c0fb5c6aebfc
Tarun <tarunharidasan2003@gmail.com>
2020-08-08 16:36:28 -0400

    Added aww.js to retrieve blessed images

---
+++ commands/aww.js
@@ -0,0 +1,21 @@
+module.exports.run = async (_client, message, args, _prefix) => {
+    //Variables
+    const fetch = require("node-fetch");
+    let url = "https://meme-api.herokuapp.com/gimme";    
+    let meme = require("../utilities/meme.js");
+
+    //Pick a random subreddit
+    let subreddits = ["Awww", "Aww"];
+    let rand = Math.round(Math.random()*subreddits.length-1);
+    url += `/${subreddits[rand]}`;
+
+    //Retrieve the meme
+    meme(url, message);
+}
+
+module.exports.help = {
+    name: ["aww"],
+    description: "Retreive a wholesome image from r/aww and r/awww",
+    page: 1,
+    title: "Fun Commands"
+}
