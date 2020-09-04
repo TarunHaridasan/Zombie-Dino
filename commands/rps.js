6a6fa32a6187fb95c011f0fa132d5accc0053862
Tarun <tarunharidasan2003@gmail.com>
2020-09-04 18:28:44 -0400

    Reformatted rps

--- commands/rps.js
+++ commands/rps.js
@@ -1,7 +1,7 @@
 module.exports.run = async (client, message, args) => {
     //Variables
     let items = ["rock", "paper", "scissors"];
-    let emoji = ["⛰️", "📄", "✂️"]
+    let emojis = ["⛰️", "📄", "✂️"]
     let userID = message.author.id;
     let Money = require("../utilities/money.js");
     let money = new Money(userID);
@@ -14,73 +14,75 @@ module.exports.run = async (client, message, args) => {
         if (!choice) throw "You must enter choice! (ex. rock, paper, or scissors)"
         if (choice!="rock" && choice!="paper" && choice!="scissors") throw "You must enter a valid choice! (ex. rock, paper, or scissors)"
         if (!bet) throw "You must enter a valid bet!"
-        if (isNaN(bet)) throw "Enter a valid bet"  
-        if (bet<10) throw "You must enter a bet greater than 10💵."
+        if (bet<=10) throw "You must enter a bet greater than 10💵."
         if (bet>money.get()) throw "You do not have that much money."
     }
     catch(err) {
-        console.log(err);
         message.channel.send({embed: {
 	        color: 0xFF0000,
 	        description: err
 	    }});
-		return false; 
+		return; 
     }
 
-    //Subtract bet
-    money.min(bet);
-
-    //Generate AI value
+    //Get computer and user choice #s
     let compChoice = Math.round(Math.random()*2);
-    console.log(compChoice);
-    choice = items.indexOf(choice);
-    console.log(choice);
+    let userChoice = items.indexOf(choice);
 
     //Calculate outcomes
-    let description = `Rock, Paper, and Scissors | ${message.author.username} - `
+    let title = `Rock, Paper, Scissors | ${message.author.username} - `
+    let description;
     let color;
-    switch(choice - compChoice) {
+    let image;
+    switch(userChoice - compChoice) {
         //Tie
         case 0:
-            money.add(bet);
-            description += `DRAW`;
+            title += `DRAW`;
             color = 0x0000FF;
+            description = "You tied. You get your bet back!";
+            image = "https://i.imgur.com/P3pwaTF.png";
             break;
         //Win
         case 1:
-            money.add(bet*2);
-            description += `YOU WIN`;
-            color = 0x00FF00;
-            break;
         case -2:
-            money.add(bet*2);
-            description += `YOU WIN`;
+            title += `YOU WIN`;
             color = 0x00FF00;
+            money.add(bet);
+            description = "You won! You won double your original bet!";
+            image = "https://i.imgur.com/JKqatwp.png";
             break;
         //Lose
         default:
-            description += `YOU LOSE`;
+            title += `YOU LOSE`;
             color = 0xFF0000;
+            money.min(bet);
+            description = "You lost! Try again next time.";
+            image = "https://i.imgur.com/ll1nMkb.png";
             break;
     }
 
     //Send message
     await message.channel.send({embed: {
-        title: description,
+        title: title,
+        description: description,
         color: color,
         fields: [
             {
                 name: "You:",
-                value: `\`${emoji[choice]} ${items[choice]}\``
+                value: `\`\`\`\n${emojis[userChoice]}- ${choice.toUpperCase()}\n\`\`\``
             },
             {
                 name: "Computer:",
-                value: `\`${emoji[compChoice]} ${items[compChoice]}\``
+                value: `\`\`\`\n${emojis[compChoice]}- ${items[compChoice].toUpperCase()}\n\`\`\``
             }
-        ]
-    }});
-
-    
+        ],
+        image: {
+            url: image
+        },
+        footer: {
+            text: `Bet: ${bet.toLocaleString()}💵`
+        }
+    }});    
 }
 
 module.exports.help = {
